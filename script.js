acba966b2d1ce3878da9ffb374be1df7171eb6b3
Cesar <cesarnsingi@hotmail.com>
2024-10-13 10:05:39 +0400

    password letter and symbol

---
+++ script.js
@@ -0,0 +1,7 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "123456789";
+const symbol = "@#$%^&*()_=~|}{[]></-=";
