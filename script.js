9c0448ab30ee52c6a8baa2b3b2e519b9993b22e3
Cesar <cesarnsingi@hotmail.com>
2024-10-13 10:23:57 +0400

    display and on click

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function createPassword(){
 
     while(length > password.length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
-
-
     }
+    passwordBox.value = password;
 }
